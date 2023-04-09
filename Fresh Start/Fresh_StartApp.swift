8f78f1bedf455e935f73ab7dfc786349ad3691ef
Marko Benacic <34174145+Markobenacic@users.noreply.github.com>
2023-04-09 15:51:35 +0200

    Initial Commit

---
+++ Fresh Start/Fresh_StartApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fresh_StartApp.swift
+//  Fresh Start
+//
+//  Created by Marko Benačić on 09.04.2023..
+//
+
+import SwiftUI
+
+@main
+struct Fresh_StartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
